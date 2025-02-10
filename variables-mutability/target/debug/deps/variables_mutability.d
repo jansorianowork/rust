5bb945ea9c310319aa3986d03188140ad33eb87f
jansorianowork <jansoriano.work@gmail.com>
2025-02-10 23:46:00 +0800

    rust: learning variables and mutability

---
+++ variables-mutability/target/debug/deps/variables_mutability.d
@@ -0,0 +1,5 @@
+D:\Work\devs\dev work\2025\web3\rust\variables-mutability\target\debug\deps\variables_mutability.exe: src\main.rs
+
+D:\Work\devs\dev work\2025\web3\rust\variables-mutability\target\debug\deps\variables_mutability.d: src\main.rs
+
+src\main.rs:
---
+++ variables-mutability/target/debug/variables-mutability.d
@@ -0,0 +1,1 @@
+D:\Work\devs\dev\ work\2025\web3\rust\variables-mutability\target\debug\variables-mutability.exe: D:\Work\devs\dev\ work\2025\web3\rust\variables-mutability\src\main.rs
